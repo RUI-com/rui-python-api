d025626885a17ef5be03bb1b9fa04741c41654b9
RUI Company <ruidotorg@gmail.com>
2025-08-10 15:38:28 +0300

    Initial commit

--- stt_script.py
+++ stt_script.py
@@ -1,5 +1,6 @@
 import sys
 import whisper
+import time
 
 if __name__ == "__main__":
     sys.stdout.reconfigure(encoding='utf-8')
@@ -10,8 +11,14 @@ if __name__ == "__main__":
 
     input_path = sys.argv[1]
 
-    model = whisper.load_model("small")
+    print("بدء تحميل الموديل...", file=sys.stderr)
+    start = time.time()
+    model = whisper.load_model("small")  # ممكن تجرب "tiny" إذا الموارد قليلة
+    print("الموديل تم تحميله.", file=sys.stderr)
+
+    print("بدء الترانسكريبت...", file=sys.stderr)
     result = model.transcribe(input_path, language="ar", temperature=0, best_of=3)
+    end = time.time()
 
-
+    print(f"مدة المعالجة: {end - start:.2f} ثانية", file=sys.stderr)
     print(result['text'].strip())
